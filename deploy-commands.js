e8f0e3d0c417fa053d9ec63ee6b2601571841cce
Robert Kalasky <kalaskyr@gmail.com>
2022-03-10 05:58:18 -0500

    initial setup

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,21 +1,17 @@
+require('dotenv').config()
 const fs = require('node:fs')
 const { REST } = require('@discordjs/rest')
 const { Routes } = require('discord-api-types/v9')
 
-const rest = new REST({ version: '9' }).setToken(token)
-
-const commands = [
-  new SlashCommandBuilder().setName('createdat').setDescription('Replies with pong!'),
-  new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-].map((command) => command.toJSON())
-
+const commands = []
 const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
   commands.push(command.data.toJSON())
 }
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
 
 rest
   .put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
